625a57e209d66cf6834608dc144487d19080802f
Wasim <wasimahmad.qc@gmail.com>
2019-02-01 22:07:04 +0100

    01 Feb Update

--- train_tonnage.py
+++ train_tonnage.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 data = pd.read_excel(r'F:\FMLP\dataset\QV Data locatie Almere_Weesp_v3_01.xlsx')
 train_tonnage_data = data.groupby(['Trein_snelheid_in'])
-train_tonnage_sum = train_tonnage_data[['Askwaliteit_aslast_ton']].sum()
+train_tonnage_mean = train_tonnage_data['Askwaliteit_aslast_ton'].mean()
