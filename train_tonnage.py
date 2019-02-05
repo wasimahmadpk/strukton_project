25376d9253ebb63419c3eb2c3147c4dabeec5159
Wasim <wasimahmad.qc@gmail.com>
2019-02-05 15:47:38 +0100

    05 Feb Update-RCF Predictive Modelling

--- train_tonnage.py
+++ train_tonnage.py
@@ -1,5 +1,36 @@
 import pandas as pd
 
-data = pd.read_excel(r'F:\FMLP\dataset\QV Data locatie Almere_Weesp_v3_01.xlsx')
-train_tonnage_data = data.groupby(['Trein_snelheid_in'])
+bigdata = pd.read_excel(r'F:\FMLP\dataset\QV Data locatie Almere_Weesp_v3_04.xlsx')
+train_tonnage_data = bigdata.groupby(['Voertuig_materieel_type'])
 train_tonnage_mean = train_tonnage_data['Askwaliteit_aslast_ton'].mean()
+train_speed_in_mean = train_tonnage_data['Trein_snelheid_in'].mean()
+train_speed_out_mean = train_tonnage_data['Trein_snelheid_uit'].mean()
+train_no_axles = train_tonnage_data['Askwaliteit_asnummer'].max()
+
+cols = []
+data_list = []
+
+for c in range(len(bigdata.columns)):
+    cols.append(bigdata.columns[c])
+count = 0
+for table, group in train_tonnage_data:
+    print('\nCREATE TABLE {}('.format(table))
+    row_data = []
+    for row, data in group.iterrows():
+        for d in range(len(data)):
+            if d == 8:
+                row_data.append(train_speed_in_mean[count])
+            elif d == 9:
+                row_data.append(train_speed_out_mean[count])
+            elif d == 11:
+                row_data.append(train_no_axles[count])
+            elif d == 12:
+                row_data.append(train_tonnage_mean[count])
+            else:
+                row_data.append(data[d])
+        break
+    count = count + 1
+    data_list.append(row_data)
+
+pdf = pd.DataFrame(data_list, columns=cols)
+pdf.to_excel(r'F:\FMLP\dataset\QV Data locatie Almere_Weesp_v3_04_processed.xlsx')
