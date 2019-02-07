0a34c803f26d4810089d12f520c5f5a54aa3a87f
Wasim <wasimahmad.qc@gmail.com>
2019-02-08 00:44:12 +0100

    07 Feb Update- algorithm evolution

--- data_processing.py
+++ data_processing.py
@@ -5,15 +5,15 @@ Created on Wed Dec  5 14:42:01 2018
 @author: Waseem
 """
 
-import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 import csv
 
+
 def pre_processing(datafile, syncfile, segfile, poifile, processedfile):
-    ## read CSV file
+
+    # read CSV file
     route_list = []
     with open(segfile) as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -21,19 +21,18 @@ def pre_processing(datafile, syncfile, segfile, poifile, processedfile):
         for row in csv_reader:
             tempStr = ''.join(row)
             if tempStr.startswith('#') or len(tempStr) == 0:
-                  continue
+                continue
             elif tempStr.startswith('PRV9_CNT_BGN'):
-                  print(f'Column names are {", ".join(row)}')
-                  line_count += 1
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
             else:
-                  #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-                  line_count += 1
-                  tlist = tempStr.split(";")
-                  route_list.append(tlist)
+                # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+                line_count += 1
+                tlist = tempStr.split(";")
+                route_list.append(tlist)
         print(f'Processed {line_count} lines in SEG file.')
         route_list = np.array(route_list)
-    
-    
+
     geo_list = []
     with open(poifile) as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -41,33 +40,28 @@ def pre_processing(datafile, syncfile, segfile, poifile, processedfile):
         for row in csv_reader:
             tempStr = ''.join(row)
             if tempStr.startswith('#') or len(tempStr) == 0:
-                  continue
+                continue
             elif tempStr.startswith('CNT'):
-                  print(f'Column names are {", ".join(row)}')
-                  line_count += 1
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
             else:
-                  #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-                  line_count += 1
-                  tlist = tempStr.split(";")
-    #              for k in range(len(tlist)):
-    #                  print(k)
-    #                  ttlist.append(float(tlist[k]))
-    #              geo_list.append(ttlist)
-                  ttlist = [float(x) for x in tlist if len(x)>0]
-                  geo_list.append(ttlist)
+                # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+                line_count += 1
+                tlist = tempStr.split(";")
+                ttlist = [float(x) for x in tlist if len(x) > 0]
+                geo_list.append(ttlist)
         print(f'Processed {line_count} lines in POI file.')
-        print ("Program is running...")
+        print("Program is running...")
         geo_list = np.array(geo_list)
         
-    lat = geo_list[:,1]
-    lon = geo_list[:,2]
+    lat = geo_list[:, 1]
+    lon = geo_list[:, 2]
+
+    # Interpolation external and geo-coordinates
+    # get_lat = interp1d(geo_list[:,0], lat, fill_value= 'extrapolate')
+    # get_long = interp1d(geo_list[:,0], lon, fill_value= 'extrapolate')
     
-    
-    ### Interpolation external and geo-coordinates
-    #get_lat = interp1d(geo_list[:,0], lat, fill_value= 'extrapolate')
-    get_long = interp1d(geo_list[:,0], lon, fill_value= 'extrapolate')
-    
-    ### documented in time tdms file
+    # documented in time tdms file
     
     syncdat = pd.read_hdf(syncfile, 'sync', mode='r')
     timedat = pd.read_hdf(datafile, 'time', mode='r', where = 'INTCNT >= syncdat.IntCnt.iloc[3] and INTCNT <= syncdat.IntCnt.iloc[-1]')
@@ -76,9 +70,37 @@ def pre_processing(datafile, syncfile, segfile, poifile, processedfile):
     xcounters = np.array(get_xcount(timedat.INTCNT))
     timedat = timedat.assign(EXTCNT=xcounters)
     
-#    newext = np.arange(np.ceil(timedat.EXTCNT[0]/10)*10,np.floor(timedat.EXTCNT[-1]/10)*10,100)
-#    distsamp = interp1d(timedat.EXTCNT,dispchab3)
-#    newdisp = distsamp(newext)
+    # newext = np.arange(np.ceil(timedat.EXTCNT[0]/10)*10,np.floor(timedat.EXTCNT[-1]/10)*10,100)
+    # distsamp = interp1d(timedat.EXTCNT,dispchab3)
+    # newdisp = distsamp(newext)
+
+    # Get rail objects location
+    obj_counters = np.array([])
+    cntval = []
+    cntstart = []
+    cntstop = []
+    with open(r'F:\strukton_project\Groningen\labeledData\temp_dist.csv') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        line_count = 0
+
+        for row in csv_reader:
+            tempStr = ''.join(row)
+            if tempStr.startswith('#') or len(tempStr) == 0:
+                continue
+            elif tempStr.startswith('Numbers'):
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                line_count += 1
+                tlist = tempStr.split(";")
+                if tlist[6] == 'D:\\Prorail\\Data\\Groningen\\171128\\Prorail17112805si12':
+                    if int(tlist[10]) == 1 or int(tlist[11]) == 1 or int(tlist[12]) == 1 or int(tlist[14]) == 1 or int(
+                            tlist[15]) == 1 or int(tlist[16]) == 1:
+                        cntstart.append(int(tlist[4]) - 500)
+                        cntstop.append(int(tlist[4]) + 500)
+        print(f'Processed {line_count} lines in rail_objects file.')
+
+    ################################################################
 
     # Populating Track and ERS values (Modified)
     edir, tdir = 0, 0
@@ -122,7 +144,7 @@ def pre_processing(datafile, syncfile, segfile, poifile, processedfile):
     CHB2 = np.array(timedat.CHB2)
     CHB3 = np.array(timedat.CHB3)
 
-    #2. Transformmed channels
+    # 2. Transformmed channels
     CHC1 = np.empty(len(tdir), dtype=object)
     CHC2 = np.empty(len(tdir), dtype=object)
     CHC3 = np.empty(len(tdir), dtype=object)
@@ -132,10 +154,10 @@ def pre_processing(datafile, syncfile, segfile, poifile, processedfile):
     CHC1[(tdir == 'Af') & (edir == 1)] = CHB1[(tdir == 'Af') & (edir == 1)]
     CHC1[(tdir == 'Af') & (edir == -1)] = CHA1[(tdir == 'Af') & (edir == -1)]
     
-#    CHC2[(tdir=='Op') & (edir==1)] = CHA2[(tdir=='Op') & (edir==1)]
-#    CHC2[(tdir=='Op') & (edir==-1)] = CHB2[(tdir=='Op') & (edir==-1)]
-#    CHC2[(tdir=='Af') & (edir==1)] = CHB2[(tdir=='Af') & (edir==1)]
-#    CHC2[(tdir=='Af') & (edir==-1)] = CHA2[(tdir=='Af') & (edir==-1)]
+    # CHC2[(tdir=='Op') & (edir==1)] = CHA2[(tdir=='Op') & (edir==1)]
+    # CHC2[(tdir=='Op') & (edir==-1)] = CHB2[(tdir=='Op') & (edir==-1)]
+    # CHC2[(tdir=='Af') & (edir==1)] = CHB2[(tdir=='Af') & (edir==1)]
+    # CHC2[(tdir=='Af') & (edir==-1)] = CHA2[(tdir=='Af') & (edir==-1)]
     
     CHC3[(tdir == 'Op') & (edir == 1)] = CHA3[(tdir == 'Op') & (edir == 1)]
     CHC3[(tdir == 'Op') & (edir == -1)] = CHB3[(tdir == 'Op') & (edir == -1)]
@@ -151,18 +173,18 @@ def pre_processing(datafile, syncfile, segfile, poifile, processedfile):
     CHD1[(tdir == 'Af') & (edir == 1)] = CHA1[(tdir == 'Af') & (edir == 1)]
     CHD1[(tdir == 'Af') & (edir == -1)] = CHB1[(tdir == 'Af') & (edir == -1)]
 
-#    CHD2[(tdir == 'Op') & (edir == 1)] = CHB2[(tdir == 'Op') & (edir == 1)]
-#    CHD2[(tdir == 'Op') & (edir == -1)] = CHA2[(tdir == 'Op') & (edir == -1)]
-#    CHD2[(tdir == 'Af') & (edir == 1)] = CHA2[(tdir == 'Af') & (edir == 1)]
-#    CHD2[(tdir == 'Af') & (edir == -1)] = CHB2[(tdir == 'Af') & (edir == -1)]
+    # CHD2[(tdir == 'Op') & (edir == 1)] = CHB2[(tdir == 'Op') & (edir == 1)]
+    # CHD2[(tdir == 'Op') & (edir == -1)] = CHA2[(tdir == 'Op') & (edir == -1)]
+    # CHD2[(tdir == 'Af') & (edir == 1)] = CHA2[(tdir == 'Af') & (edir == 1)]
+    # CHD2[(tdir == 'Af') & (edir == -1)] = CHB2[(tdir == 'Af') & (edir == -1)]
 
     CHD3[(tdir == 'Op') & (edir == 1)] = CHB3[(tdir == 'Op') & (edir == 1)]
     CHD3[(tdir == 'Op') & (edir == -1)] = CHA3[(tdir == 'Op') & (edir == -1)]
     CHD3[(tdir == 'Af') & (edir == 1)] = CHA3[(tdir == 'Af') & (edir == 1)]
     CHD3[(tdir == 'Af') & (edir == -1)] = CHB3[(tdir == 'Af') & (edir == -1)]
 
-    #LAT = np.array(get_lat(xcounters))
-    #LON = np.array(get_long(xcounters))
+    # LAT = np.array(get_lat(xcounters))
+    # LON = np.array(get_long(xcounters))
 
     timedat = timedat.drop(['CHA1', 'CHA2', 'CHA3', 'CHB1', 'CHB2', 'CHB3'], axis = 1)
     timedat = timedat.assign(CHC1=CHC1, CHC3=CHC3, CHD1=CHD1, CHD3=CHD3)
@@ -173,10 +195,24 @@ def pre_processing(datafile, syncfile, segfile, poifile, processedfile):
         temparr = np.array(timedat[(timedat.EXTCNT >= switch_start[z]) & (timedat.EXTCNT <= switch_end[z])].index)
         switch_counters = np.concatenate((switch_counters, temparr), axis=0)
 
-    timedat.CHC1[list(switch_counters)] = np.mean(timedat.CHC1)
-    timedat.CHC3[list(switch_counters)] = np.mean(timedat.CHC3)
-    timedat.CHD1[list(switch_counters)] = np.mean(timedat.CHD1)
-    timedat.CHD3[list(switch_counters)] = np.mean(timedat.CHD3)
+    chc1_mean = np.mean(timedat.CHC1)
+    chc3_mean = np.mean(timedat.CHC3)
+    chd1_mean = np.mean(timedat.CHD1)
+    chd3_mean = np.mean(timedat.CHD3)
+
+    timedat.CHC1[list(switch_counters)] = chc1_mean
+    timedat.CHC3[list(switch_counters)] = chc3_mean
+    timedat.CHD1[list(switch_counters)] = chd1_mean
+    timedat.CHD3[list(switch_counters)] = chd3_mean
+
+    for z in range(len(cntstart)):
+        temparr = np.array(timedat[(timedat.EXTCNT >= cntstart[z]) & (timedat.EXTCNT <= cntstop[z])].index)
+        obj_counters = np.concatenate((obj_counters, temparr), axis=0)
+
+    timedat.CHC1[list(obj_counters)] = chc1_mean
+    timedat.CHC3[list(obj_counters)] = chc3_mean
+    timedat.CHD1[list(obj_counters)] = chd1_mean
+    timedat.CHD3[list(obj_counters)] = chd3_mean
 
     # timedat = timedat.drop(list(switch_counters), axis=0)
     timedat.to_hdf(processedfile, key='processed', mode='w')
--- rail_defects_main.py
+++ rail_defects_main.py
@@ -59,56 +59,14 @@ else:
         print(f'Processed {line_count} lines in POI file.')
         geo_list = np.array(geo_list)
         
-    lat = geo_list[:,1]
-    lon = geo_list[:,2]
-    
-    
-    ### Interpolation external and geo-coordinates
-    get_lat = interp1d(geo_list[:,0], lat, fill_value= 'extrapolate')
-    get_long = interp1d(geo_list[:,0], lon, fill_value= 'extrapolate')
-    
-    ## Get rail objects location
-    obj_counters = np.array([])
-    cntval = []
-    cntstart = []
-    cntstop = []
-    with open(r'F:\strukton_project\Groningen\labeledData\temp_dist.csv') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        line_count = 0
-        
-        for row in csv_reader:
-            tempStr = ''.join(row)
-            if tempStr.startswith('#') or len(tempStr) == 0:
-                  continue
-            elif tempStr.startswith('Numbers'):
-                  print(f'Column names are {", ".join(row)}')
-                  line_count += 1
-            else:
-                  line_count += 1
-                  tlist = tempStr.split(";")
-                  if tlist[6]== 'D:\\Prorail\\Data\\Groningen\\171128\\Prorail17112805si12':
-                      if int(tlist[10])==1 or int(tlist[11])==1 or int(tlist[12])==1 or int(tlist[14])==1 or int(tlist[15])==1 or int(tlist[16])==1:
-                          cntstart.append(int(tlist[4]) - 500)
-                          cntstop.append(int(tlist[4]) + 500)
-#                          temparr = np.array(list(range(cntstart, cntstop, 1)))
-#                          obj_counters = np.concatenate((obj_counters, temparr), axis=0)
-#                  
-        print(f'Processed {line_count} lines in rail_objects file.')
-        print ("Program is running...")
+    lat = geo_list[:, 1]
+    lon = geo_list[:, 2]
 
-    
-    ################################################################
+    # Interpolation external and geo-coordinates
+    get_lat = interp1d(geo_list[:, 0], lat, fill_value='extrapolate')
+    get_long = interp1d(geo_list[:, 0], lon, fill_value='extrapolate')
+
     processed_data = pd.read_hdf(processed_file, 'processed', mode='r')
-    
-    for z in range(len(cntstart)):
-        temparr = np.array(processed_data[(processed_data.EXTCNT >= cntstart[z]) & (processed_data.EXTCNT <= cntstop[z])].index)
-        obj_counters = np.concatenate((obj_counters, temparr), axis=0)
-
-    processed_data.CHC1[list(obj_counters)] = np.mean(processed_data.CHC1)
-    processed_data.CHC3[list(obj_counters)] = np.mean(processed_data.CHC3)
-    processed_data.CHD1[list(obj_counters)] = np.mean(processed_data.CHD1)
-    processed_data.CHD3[list(obj_counters)] = np.mean(processed_data.CHD3)
-    # processed_data = processed_data.drop(list(obj_counters), axis=0)
     
     CHC1 = np.array(processed_data.CHC1)
     CHC3 = np.array(processed_data.CHC3)
@@ -118,32 +76,31 @@ else:
     
     int_count = np.array(processed_data.INTCNT)
     ext_count = np.array(processed_data.EXTCNT)
-#    date_time = syncdat.DateTime
+    # date_time = syncdat.DateTime
+
+    # Pushing & Pulling ABA data for one side (left or right)
+    pull_data_chc1 = CHC1[(EDIR == 1)]
+    push_data_chc1 = CHC1[(EDIR == -1)]
+    pull_data_chc3 = CHC3[(EDIR == 1)]
+    push_data_chc3 = CHC3[(EDIR == -1)]
     
+    pull_int_count = int_count[(EDIR == 1)]
+    push_int_count = int_count[(EDIR == -1)]
     
-    #Pushing & Pulling ABA data for one side (left or right)
-    pull_data_chc1 = CHC1[(EDIR==1)]
-    push_data_chc1 = CHC1[(EDIR==-1)]
-    pull_data_chc3 = CHC3[(EDIR==1)]
-    push_data_chc3 = CHC3[(EDIR==-1)]
+    pull_data = np.power((np.power(pull_data_chc1, 2) + np.power(pull_data_chc3, 2)), 1/2)
+    push_data = np.power((np.power(push_data_chc1, 2) + np.power(push_data_chc3, 2)), 1/2)
     
-    pull_int_count = int_count[(EDIR==1)]
-    push_int_count = int_count[(EDIR==-1)]
+    # Second side
+    pull_data_chc1 = CHD1[(EDIR == 1)]
+    push_data_chc1 = CHD1[(EDIR == -1)]
+    pull_data_chc3 = CHD3[(EDIR == 1)]
+    push_data_chc3 = CHD3[(EDIR == -1)]
     
-    pull_data = np.power((np.power(pull_data_chc1,2) + np.power(pull_data_chc3,2)), 1/2)
-    push_data = np.power((np.power(push_data_chc1,2) + np.power(push_data_chc3,2)), 1/2)
+    pull_int_count = int_count[(EDIR == 1)]
+    push_int_count = int_count[(EDIR == -1)]
     
-    ## Second side
-    pull_data_chc1 = CHD1[(EDIR==1)]
-    push_data_chc1 = CHD1[(EDIR==-1)]
-    pull_data_chc3 = CHD3[(EDIR==1)]
-    push_data_chc3 = CHD3[(EDIR==-1)]
-    
-    pull_int_count = int_count[(EDIR==1)]
-    push_int_count = int_count[(EDIR==-1)]
-    
-    pull_data2 = np.power((np.power(pull_data_chc1,2) + np.power(pull_data_chc3,2)), 1/2)
-    push_data2 = np.power((np.power(push_data_chc1,2) + np.power(push_data_chc3,2)), 1/2)
+    pull_data2 = np.power((np.power(pull_data_chc1, 2) + np.power(pull_data_chc3, 2)), 1/2)
+    push_data2 = np.power((np.power(push_data_chc1, 2) + np.power(push_data_chc3, 2)), 1/2)
     
     ########################################
     rail_data = []
@@ -167,19 +124,19 @@ else:
     anom_xcount_list = []
     anom_xcount_train_list = []
     
-    get_xcount = interp1d(int_count, ext_count, fill_value= 'extrapolate')
-    get_icount = interp1d(ext_count, int_count, fill_value= 'extrapolate')
-    #///////////// Feature Extraction //////////////
+    get_xcount = interp1d(int_count, ext_count, fill_value='extrapolate')
+    get_icount = interp1d(ext_count, int_count, fill_value='extrapolate')
 
+    # ///////////// Feature Extraction //////////////
     aba_data_side = []
     all_xcount_mode = []
-    for i in range(len(rail_data)):
+    for i in range(len(rail_data) - 1):
         aba_data_mode = []
         int_count_mode = []
-        for j in range(len(data_list)):
-            input = rail_data[i]
+        for j in range(len(data_list) - 1):
+            input_data = rail_data[i]
             counters = rail_counters[i]
-            list_of_features = extract_features(input[j], counters[j], 1500)
+            list_of_features = extract_features(input_data[j], counters[j], 1500)
 
             rms = np.array(list_of_features[:, 0])
             kurtosis = np.array(list_of_features[:, 2])
@@ -213,68 +170,64 @@ else:
             plt.plot(crest_factor)
             plt.show()
 
-            mylist = np.stack((peak_to_peak, crest_factor, rms, skewness, kurtosis, impulse_factor), axis=-1)
+            mylist = np.stack((rms, kurtosis), axis=-1)
 
-            #best_k(mylist)
-            #//////K-Means Clustering and Data Labelling for Supervised Anomaly detection///////////////
-            #norm_data = []
-            #anom_data = []
-            #kmeans = KMeans(n_clusters=2, random_state=1).fit(mylist)
-            #clusters = np.array(kmeans.labels_.astype(float))
-            #for i in range(len(clusters)):
+            # best_k(mylist)
+            # //////K-Means Clustering and Data Labelling for Supervised Anomaly detection///////////////
+            # norm_data = []
+            # anom_data = []
+            # kmeans = KMeans(n_clusters=2, random_state=1).fit(mylist)
+            # clusters = np.array(kmeans.labels_.astype(float))
+            # for i in range(len(clusters)):
             #    if clusters[i] == 0:
             #        norm_data.append(list_of_features[i,:])
-            ##        plt.scatter(list_of_features[i,0],list_of_features[i,4], c = 'g', marker = '*')
-            ##        plt.title("Data Clustering")
-            ##        plt.xlabel("Feature 1")
-            ##        plt.ylabel("Feature 2")
-            ##        plt.show()
+            #        plt.scatter(list_of_features[i,0],list_of_features[i,4], c = 'g', marker = '*')
+            #        plt.title("Data Clustering")
+            #        plt.xlabel("Feature 1")
+            #        plt.ylabel("Feature 2")
+            #        plt.show()
             #    elif clusters[i] ==1:
             #        anom_data.append(list_of_features[i,:])
-            ##        plt.scatter(list_of_features[i,0],list_of_features[i,4], c = 'r', marker = 'o')
-            ##        plt.title("Data Clustering")
-            ##        plt.xlabel("Feature 1")
-            ##        plt.ylabel("Feature 2")
-            ##        plt.show()
-            #
-            #
-            #norm_data = np.array(norm_data)
-            #anom_data = np.array(anom_data)
+            #        plt.scatter(list_of_features[i,0],list_of_features[i,4], c = 'r', marker = 'o')
+            #        plt.title("Data Clustering")
+            #        plt.xlabel("Feature 1")
+            #        plt.ylabel("Feature 2")
+            #        plt.show()
 
-            #X_train = np.concatenate((norm_data, anom_data), axis=0)
+            # norm_data = np.array(norm_data)
+            # anom_data = np.array(anom_data)
 
-            #multi_anomaly_detection(norm_data, anom_data)
+            # X_train = np.concatenate((norm_data, anom_data), axis=0)
+            # multi_anomaly_detection(norm_data, anom_data)
 
             norm_train, anom_train, norm_test, anom_test, anom_icount, anom_icount_train = isolation_forest(mylist, int_count)
 
-        #    norm_train = np.concatenate(norm_train.tolist())
-        #    anom_train = np.concatenate(anom_train.tolist())
-        #    norm_test = np.concatenate(norm_test.tolist())
-        #    anom_test = np.concatenate(anom_test.tolist())
+            # norm_train = np.concatenate(norm_train.tolist())
+            # anom_train = np.concatenate(anom_train.tolist())
+            # norm_test = np.concatenate(norm_test.tolist())
+            # anom_test = np.concatenate(anom_test.tolist())
 
             all_xcount_mode.append(get_xcount(int_count))
             anom_xcount_test = get_xcount(anom_icount)
             anom_xcount_train = get_xcount(anom_icount_train)
-            anom_xcount = np.concatenate((anom_xcount_train, anom_xcount_test),axis=0)
+            anom_xcount = np.concatenate((anom_xcount_train, anom_xcount_test), axis=0)
 
-            #new code for validation of anomalies
+            # new code for validation of anomalies
             latitude = get_lat(anom_xcount)
             longitude = get_long(anom_xcount)
             dist = [0]
+
             for z in range(len(latitude)-1):
                 point_one = (latitude[z], longitude[z])
                 point_two = (latitude[z+1], longitude[z+1])
                 distance = geodesic(point_one, point_two).km
                 dist.append(1000 * distance)
 
-
             anom_xcount = [str(x) for x in anom_xcount]
             latitude = [str(x) for x in latitude]
             longitude = [str(x) for x in longitude]
             dist = [str(x) for x in dist]
 
-    #        d = [list(anom_xcount), list(latitude), list(longitude), dist]
-    #        write_data = zip_longest(*d, fillvalue = '')
             write_data = zip(anom_xcount, latitude, longitude, dist)
             track_side = 'right' if i else 'left'
             train_mode = 'pushing' if j else 'pulling'
@@ -282,13 +235,12 @@ else:
             with open(counters_path + '\prorail17112805si12_' + track_side + '_' + train_mode + '.csv', 'w', newline='') as file:
                 try:
                     writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                    writer.writerow(['counters', 'latitude','longitude','distance'])
+                    writer.writerow(['counters', 'latitude', 'longitude', 'distance'])
                     for cnt, lat, lon, dist in write_data:
                         writer.writerow([cnt, lat, lon, dist])
                 finally:
                     file.close()
             #######################################################
-
 
             anom_xcount_list.append(anom_xcount)
             anom_xcount_train_list.append(anom_xcount_train)
@@ -300,25 +252,25 @@ else:
 
             #    dlist = []
             #    xcgcode = []
-            #
-            #geocodes_train = np.array(geocode)
-            #unique_gcodes_train = np.unique(geocodes_train, axis = 0)
-            #lat_list_train = unique_gcodes_train[:, 1].tolist()
-            #long_list_train = unique_gcodes_train[:, 2].tolist()
-            #
-            #lat_list_train = [float(i) for i in lat_list_train]
-            #long_list_train = [float(j) for j in lon_list_train]
+
+            # geocodes_train = np.array(geocode)
+            # unique_gcodes_train = np.unique(geocodes_train, axis = 0)
+            # lat_list_train = unique_gcodes_train[:, 1].tolist()
+            # long_list_train = unique_gcodes_train[:, 2].tolist()
+
+            # lat_list_train = [float(i) for i in lat_list_train]
+            # long_list_train = [float(j) for j in lon_list_train]
 
             # Calculation of time of data acquisition
 
-            #geo_xcount = geo_list[:,0]
-            #geo_icount = get_icount(geo_xcount)
-            #geo_icount[np.isnan(geo_icount)] = 0
+            # geo_xcount = geo_list[:,0]
+            # geo_icount = get_icount(geo_xcount)
+            # geo_icount[np.isnan(geo_icount)] = 0
 
-            #dlist = []
-            #dtval = []
-            #
-            #for j in range(len(geo_icount)):
+            # dlist = []
+            # dtval = []
+
+            # for j in range(len(geo_icount)):
             #    tval1 = geo_icount[j]
             #    for k in range(len(int_count2)):
             #        tval2 = int(int_count2[k])
@@ -332,19 +284,19 @@ else:
             #    dlist = []
             #    xcgcode = []
             #
-            #dtval = np.array(dtval)
-            #unique_dtval = np.unique(dtval, axis=0)
+            # dtval = np.array(dtval)
+            # unique_dtval = np.unique(dtval, axis=0)
 
             gmap_plot(lat_list_train + lat_list, long_list_train + long_list)
 
-            #///// Plot Confusion Matrix /////////////
+            # ///// Plot Confusion Matrix /////////////
 
-            #YTrue = np.concatenate((X_true_test, X_true_outliers), axis=0)
-            #YPred = np.concatenate((y_pred_test, y_pred_outliers), axis=0)
-            #
-            #conf_mat = confusion_matrix(YTrue, YPred)
-            ## Plot non-normalized confusion matrix
-            #plt.figure()
-            #plot_confusion_matrix(conf_mat, classes= ['Normal', 'Anomaly'],
+            # YTrue = np.concatenate((X_true_test, X_true_outliers), axis=0)
+            # YPred = np.concatenate((y_pred_test, y_pred_outliers), axis=0)
+
+            # conf_mat = confusion_matrix(YTrue, YPred)
+            # Plot non-normalized confusion matrix
+            # plt.figure()
+            # plot_confusion_matrix(conf_mat, classes= ['Normal', 'Anomaly'],
             #                      title='Confusion matrix')
         aba_data_side.append(aba_data_mode)
