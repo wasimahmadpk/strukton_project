4f019413bb9711751d398065f04b67e75cecfff1
Wasim <wasimahmad.qc@gmail.com>
2019-03-25 19:45:37 +0100

    25 March Update - Passage comparison, Severity analysis, UI design (PyQt5)

---
+++ severity_analysis.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+class defect_severity(severity_data, ect_datapath):
+
+    def __init__(self):
+        a = 2
+
+    def get_trend(self):
+        ab = 2
+        return ab
