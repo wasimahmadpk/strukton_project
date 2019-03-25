99b4aec61d5f947ee96edf02c9effc296a5c4383
Wasim <wasimahmad.qc@gmail.com>
2019-03-25 17:30:18 +0100

    25 March Update - Passage comparison, UI design (PyQt5)

--- rail_defects_main.py
+++ rail_defects_main.py
@@ -299,7 +299,7 @@ else:
     # Data-frame for severity analysis
 
     dict = {'position': anom_pos_cha, 'counters': anom_pos_cha, 'score': anom_score_cha}
-    df_anom_pos_score = pd.dataframe(data=dict)
+    df_anom_pos_score = pd.DataFrame(data=dict)
 
     ##################################
 
