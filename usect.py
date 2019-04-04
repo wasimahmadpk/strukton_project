398be126537d7d39315026e5324c57310ea74eea
Wasim <wasimahmad.qc@gmail.com>
2019-04-04 23:04:55 +0200

    04 April- Processing Strukton US ECT Dataset

--- usect.py
+++ usect.py
@@ -5,6 +5,6 @@ df = df[(df['UIC foutcode'] == 2223) & (df['Been'] == 'L')]
 df_tracks = df.groupby('ObjectOms', as_index=False).count().sort_values('Been', ascending=False).head(10)
 toptentracks = df_tracks['ObjectOms'].iloc[0:-1].tolist()
 dframe = df[df['ObjectOms'].isin(toptentracks)]
-dframe = dframe[dframe['ObjectOms'] != 'LEEG']
-
+dframe = dframe[dframe['ObjectOms'] != 'LEEG'].sort_values('ObjectOms')
+dframe.groupby('ObjectOms').apply(lambda x: x.sort_values(['Datum']))
 dframe.to_excel(r'F:\strukton_project\USECT\US_TOTAL_PROCESSED.xlsx')
